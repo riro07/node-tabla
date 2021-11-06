2bd2e3affa3e8f079613013ec786f4000aa3c7af
Gabi <gabrielleonardomendoza19@gmail.com>
2021-11-05 22:10:42 -0300

    Optimizacion de codigo

--- app.js
+++ app.js
@@ -16,6 +16,6 @@ console.log('base: yargs', argv.b)
 
 
 crearArchivo( argv.b, argv.l, argv.h )
-    .then( a => console.log( a ))
+    .then( a => console.log( a, 'By gabi' ))
     .catch( e => console.log( e ));
 
